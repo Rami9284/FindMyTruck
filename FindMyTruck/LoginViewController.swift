957445880d6b43269571b15816858d17356e3905
chino abatol <chinoabatol@chinos-Mac.local>
2019-04-06 19:55:51 -0700

    readding all files

---
+++ FindMyTruck/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginViewController.swift
+//  FindMyTruck
+//
+//  Created by chino abatol on 4/6/19.
+//  Copyright © 2019 chino abatol. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FindMyTruck/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FindMyTruck
+//
+//  Created by chino abatol on 4/6/19.
+//  Copyright © 2019 chino abatol. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
